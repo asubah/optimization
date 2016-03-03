fa25f55d79342e59973506db2c1b5861991386fc
xatta6 <abdulla.e.subah@gmail.com>
2016-03-03 14:15:31 +0300

    * indentation and reformatting * Making the matrix symmetric

--- Traveling Salesman/generate_m.py
+++ Traveling Salesman/generate_m.py
@@ -3,24 +3,25 @@
 import random
 import numpy as np
 
-matrix = None
 
 def generate(n, mini=1, maxi=20, seed=None):
     if not seed:
         random.seed(seed)
-    
+
     m = []
     for i in range(0, n):
         r = []
-        
+
         for j in range(0, n):
             if j < i:
                 r.append(random.randint(mini, maxi))
             else:
                 r.append(0)
-        
-        m.append(r) 
-    
+
+        m.append(r)
+
     matrix = np.matrix(m, dtype=np.dtype(int))
+    matrix += matrix.transpose()
+
     np.savetxt('m.txt', matrix, fmt='%i')
     return matrix
