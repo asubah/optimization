c8541373955cada88d65a78bea9c33cf728a3273
xatta6 <abdulla.e.subah@gmail.com>
2016-04-12 20:17:21 +0300

    Implementing Method 3

--- Traveling Salesman/ts.py
+++ Traveling Salesman/ts.py
@@ -1,9 +1,6 @@
 #!usr/bin/python3
 
-from operator import itemgetter
-from sys import maxsize
 from copy import deepcopy
-import numpy as np
 import os
 import re
 
@@ -29,7 +26,7 @@ def method_0():
 
         print("========", "NEAREST NEIGHBOUR", "========")
         original_tour, original_cost = tm.ts_nearest_neighbour(m)
-        result.write('%d ' % best_cost.item())
+        result.write('%d ' % original_cost.item())
 
         # print("========", "MEAN", "========")
         # tour, best_cost = tm.ts_mean(m)
@@ -65,7 +62,7 @@ def method_1():
     instances = os.listdir(path)
     # instances.sort()
 
-    result = open('results_1.csv', 'w')
+    result = open('results_1_1.csv', 'w')
     result.write('INSTANCE N NEAREST_NEIGHBOUR Result Trace\n')
 
     for instance in instances:
@@ -147,7 +144,7 @@ def method_2():
     instances = os.listdir(path)
     # instances.sort()
 
-    result = open('results_2.csv', 'w')
+    result = open('results_2_1.csv', 'w')
     result.write('INSTANCE N NEAREST_NEIGHBOUR Result\n')
 
     for instance in instances:
@@ -196,8 +193,97 @@ def method_2():
     result.close()
 
 
+def method_3():
+    path = 'benchmarks/'
+
+    instances = os.listdir(path)
+    # instances = ['bayg29.txt']
+    # instances.sort()
+
+    for instance in instances:
+        result = open('Instances/' + instance + '.csv', 'w')
+        result.write('I NEAREST FINAL TOUR\n')
+
+        # trace = ''
+        # result.write(instance + ' ' + re.search('(\d+)', instance).group(0) + ' ')
+        tu.produce_matrix(path + instance)
+        m = tu.read_matrix()
+
+        print('INSTANCE:', instance)
+
+        for i in range(0, m.shape[0]):
+            print("========", "NEAREST NEIGHBOUR", "========")
+            initial_tour, initial_cost = tm.ts_nearest_neighbour_single(i, m)
+            result.write('%i %d ' % (i, initial_cost.item()))
+
+            # print("========", "MEAN", "========")
+            # tour, best_cost = tm.ts_mean(m)
+
+            best_tour = initial_tour
+            best_cost = initial_cost
+
+            while True:
+                # t = ''
+                print("========", "0101", "========")
+                tour = deepcopy(initial_tour)
+                tour_0101, cost_0101 = tm.ts_0101(m, tour, initial_cost)
+                # result.write('%d ' % cost_0101.item())
+
+                if cost_0101 < best_cost:
+                    best_cost = cost_0101
+                    best_tour = deepcopy(tour_0101)
+                    # t = '0101,'
+
+                print("========", "1010", "========")
+                tour = deepcopy(initial_tour)
+                tour_1010, cost_1010 = tm.ts_1010(m, tour, initial_cost)
+                # result.write('%d ' % cost_1010.item())
+
+                if cost_1010 < best_cost:
+                    best_cost = cost_1010
+                    best_tour = deepcopy(tour_1010)
+                    # t = '1010,'
+
+                print("========", "0011", "========")
+                tour = deepcopy(initial_tour)
+                tour_0011, cost_0011 = tm.ts_0011(m, tour, initial_cost)
+                # result.write('%d ' % cost_0011.item())
+
+                if cost_0011 < best_cost:
+                    best_cost = cost_0011
+                    best_tour = deepcopy(tour_0011)
+                    # t = '0011,'
+
+                print("========", "1100", "========")
+                tour = deepcopy(initial_tour)
+                tour_1100, cost_1100 = tm.ts_1100(m, tour, initial_cost)
+                # result.write('%d ' % cost_1100.item())
+
+                if cost_1100 < best_cost:
+                    best_cost = cost_1100
+                    best_tour = deepcopy(tour_1100)
+                    # t = '1100,'
+
+                if best_cost < initial_cost:
+                    initial_cost = best_cost
+                    initial_tour = deepcopy(best_tour)
+                    # trace += t
+                else:
+                    result.write('%d ' % best_cost.item())
+
+                    result.write('%d ' % best_tour[0][0][0])
+                    for edge in best_tour:
+                        result.write('%d ' % edge[0][1])
+
+                    break
+
+            result.write('\n')
+
+        result.close()
+
+
 def main():
-    method_1()
+    method_3()
 
 if __name__ == "__main__":
     main()
--- Traveling Salesman/ts_methods.py
+++ Traveling Salesman/ts_methods.py
@@ -74,259 +74,294 @@ def ts_nearest_neighbour(m):
     return best_tour, best_cost
 
 
+def ts_nearest_neighbour_single(i, m):
+    best_cost = maxsize
+    best_tour = None
+
+    # for i in range(0, m.shape[0]):
+    visited = [i]
+    mini_i = i
+    for x in range(0, m.shape[0] - 1):
+        row = deepcopy(m[mini_i])
+
+        for j in visited:
+            row[0, j] = maxsize
+
+        mini = row.min()
+        mini_i = row.argmin()
+
+        # print(mini, mini_i)
+        visited.append(mini_i)
+
+    tour = tu.array_to_tour(m, visited)
+
+    new_cost = tu.tour_cost_sorted(tour)
+    # tu.print_tour_sorted(tour)
+
+    print('Total Cost: ', new_cost)
+
+    if new_cost < best_cost:
+        best_cost = new_cost
+        best_tour = deepcopy(tour)
+
+    print('Best Cost Reached:', best_cost)
+
+    return best_tour, best_cost
+
+
 def ts_0011(m, tour, best_cost):
-    best_tour = deepcopy(tour)
+    # best_tour = deepcopy(tour)
 
-    while True:
-        sorted_tour = sorted(tour, key=itemgetter(1), reverse=True)
+    # while True:
+    sorted_tour = sorted(tour, key=itemgetter(1), reverse=True)
 
-        first_max = sorted_tour[0]
-        second_max = None
-        for (edge, cost) in sorted_tour[1:]:
-            if not tu.are_neighbours(first_max[0], edge):
-                second_max = (edge, cost)
-                break
-            else:
-                continue
-
-        new_edge1 = (first_max[0][0], second_max[0][0])
-        new_edge2 = (first_max[0][1], second_max[0][1])
-
-        tour.pop(tour.index(first_max))
-        tour.pop(tour.index(second_max))
-        tour.append((new_edge1, m[new_edge1]))
-        tour.append((new_edge2, m[new_edge2]))
-
-        tour = tu.sort_tour_list(m, tour)
-
-        new_cost = tu.tour_cost_sorted(tour)
-        # tu.print_tour_sorted(tour)
-
-        print('Total Cost: ', new_cost)
-
-        if new_cost < best_cost:
-            best_cost = new_cost
-            best_tour = deepcopy(tour)
-        else:
-            print('Best Cost Reached:', best_cost)
-
+    first_max = sorted_tour[0]
+    second_max = None
+    for (edge, cost) in sorted_tour[1:]:
+        if not tu.are_neighbours(first_max[0], edge):
+            second_max = (edge, cost)
             break
-
-    return best_tour, best_cost
-
-
-def ts_0101(m, tour, best):
-    best_cost = best
-    best_tour = deepcopy(tour)
-
-    while True:
-        max_edge = sorted(tour, key=itemgetter(1), reverse=True)[0]
-
-        neighbour1, neighbour2 = tu.get_neighbour_edges(tour, max_edge)
-        node0, node1 = max_edge[0][0], max_edge[0][1]
-        node0_vals, node1_vals = deepcopy(m[node0]), deepcopy(m[node1])
-
-        node0_vals[0, neighbour1[0][0]] = maxsize
-        node0_vals[0, node0] = maxsize
-        node0_vals[0, node1] = maxsize
-
-        node1_vals[0, neighbour2[0][1]] = maxsize
-        node1_vals[0, node1] = maxsize
-        node1_vals[0, node0] = maxsize
-
-        node0_min = node0_vals.min()
-        node1_min = node1_vals.min()
-
-        selected_node = None
-        new_node = None
-        if node0_min < node1_min:
-            selected_node = node0
-            new_node = node0_vals.argmin()
-
-            tour.pop(tour.index(max_edge))
-            added = ((selected_node, new_node), m[(selected_node, new_node)])
-            tour.append(added)
-            removed_edge = tu.get_edge(tour, node_x=new_node)
-            tour.pop(tour.index(removed_edge))
-            added = ((node1, removed_edge[0][1]), m[(node1, removed_edge[0][1])])
-            tour.append(added)
         else:
-            selected_node = node1
-            new_node = node1_vals.argmin()
+            continue
 
-            tour.pop(tour.index(max_edge))
-            added = ((selected_node, new_node), m[(selected_node, new_node)])
-            tour.append(added)
-            removed_edge = tu.get_edge(tour, node_y=new_node)
-            tour.pop(tour.index(removed_edge))
-            added = ((node0, removed_edge[0][0]), m[(node0, removed_edge[0][0])])
-            tour.append(added)
+    new_edge1 = (first_max[0][0], second_max[0][0])
+    new_edge2 = (first_max[0][1], second_max[0][1])
 
-        tour = tu.sort_tour_list(m, tour)
+    tour.pop(tour.index(first_max))
+    tour.pop(tour.index(second_max))
+    tour.append((new_edge1, m[new_edge1]))
+    tour.append((new_edge2, m[new_edge2]))
 
-        new_cost = tu.tour_cost_sorted(tour)
-        # tu.print_tour_sorted(tour)
+    tour = tu.sort_tour_list(m, tour)
 
-        print('Total Cost: ', new_cost)
+    new_cost = tu.tour_cost_sorted(tour)
+    # tu.print_tour_sorted(tour)
 
-        if new_cost < best_cost:
-            best_cost = new_cost
-            best_tour = deepcopy(tour)
-        else:
-            print('Best Cost Reached:', best_cost)
-            break
+    print('Total Cost: ', new_cost)
 
-    return best_tour, best_cost
+    if new_cost < best_cost:
+        best_cost = new_cost
+    #     best_tour = deepcopy(tour)
+    # else:
+    #     print('Best Cost Reached:', best_cost)
+    #
+    #     break
+
+    return tour, best_cost
 
 
-def ts_1100(m, tour, best):
-    best_cost = best
-    best_tour = deepcopy(tour)
+def ts_0101(m, tour, best_cost):
+    # best_cost = best
+    # best_tour = deepcopy(tour)
 
-    while True:
-        m_copy = deepcopy(m)
+    # while True:
+    max_edge = sorted(tour, key=itemgetter(1), reverse=True)[0]
 
-        for edge in tour:
-            m_copy[edge[0]] = maxsize
-            m_copy[edge[0][1], edge[0][0]] = maxsize
-            m_copy[edge[0][0], edge[0][0]] = maxsize
+    neighbour1, neighbour2 = tu.get_neighbour_edges(tour, max_edge)
+    node0, node1 = max_edge[0][0], max_edge[0][1]
+    node0_vals, node1_vals = deepcopy(m[node0]), deepcopy(m[node1])
 
-        min_cost = m_copy.min()
-        min_index = np.where(m_copy == min_cost)[0]
-        min_edge = ((min_index[0], min_index[1]), min_cost)
+    node0_vals[0, neighbour1[0][0]] = maxsize
+    node0_vals[0, node0] = maxsize
+    node0_vals[0, node1] = maxsize
 
-        neighbours1 = tu.get_neighbour_nodes(tour, min_index[0])
-        neighbours2 = tu.get_neighbour_nodes(tour, min_index[1])
-        # print(neighbours1)
-        # print(neighbours2)
-        # print(neighbours1[0][0], neighbours2[0][0])
-        # print(neighbours1[1][1], neighbours2[1][1])
+    node1_vals[0, neighbour2[0][1]] = maxsize
+    node1_vals[0, node1] = maxsize
+    node1_vals[0, node0] = maxsize
 
-        cost1 = m[neighbours1[0][0], neighbours2[0][0]]
-        cost2 = m[neighbours1[1][1], neighbours2[1][1]]
+    node0_min = node0_vals.min()
+    node1_min = node1_vals.min()
 
-        min_edge2 = None
-        if cost1 < cost2:
-            min_edge2 = ((neighbours1[0][0], neighbours2[0][0]), cost1)
-        else:
-            min_edge2 = ((neighbours1[1][1], neighbours2[1][1]), cost2)
+    selected_node = None
+    new_node = None
+    if node0_min < node1_min:
+        selected_node = node0
+        new_node = node0_vals.argmin()
 
-        # print(min_edge)
-        # print(min_edge2)
+        tour.pop(tour.index(max_edge))
+        added = ((selected_node, new_node), m[(selected_node, new_node)])
+        tour.append(added)
+        removed_edge = tu.get_edge(tour, node_x=new_node)
+        tour.pop(tour.index(removed_edge))
+        added = ((node1, removed_edge[0][1]), m[(node1, removed_edge[0][1])])
+        tour.append(added)
+    else:
+        selected_node = node1
+        new_node = node1_vals.argmin()
 
-        tour.append(min_edge)
-        tour.append(min_edge2)
+        tour.pop(tour.index(max_edge))
+        added = ((selected_node, new_node), m[(selected_node, new_node)])
+        tour.append(added)
+        removed_edge = tu.get_edge(tour, node_y=new_node)
+        tour.pop(tour.index(removed_edge))
+        added = ((node0, removed_edge[0][0]), m[(node0, removed_edge[0][0])])
+        tour.append(added)
 
-        to_be_removed = ((min_edge[0][0], min_edge2[0][0]), m[(min_edge[0][0], min_edge2[0][0])])
+    tour = tu.sort_tour_list(m, tour)
+
+    new_cost = tu.tour_cost_sorted(tour)
+    # tu.print_tour_sorted(tour)
+
+    print('Total Cost: ', new_cost)
+
+    if new_cost < best_cost:
+        best_cost = new_cost
+    #     best_tour = deepcopy(tour)
+    # else:
+    #     print('Best Cost Reached:', best_cost)
+    #     break
+
+    return tour, best_cost
+
+
+def ts_1100(m, tour, best_cost):
+    # best_cost = best
+    # best_tour = deepcopy(tour)
+
+    # while True:
+    m_copy = deepcopy(m)
+
+    for edge in tour:
+        m_copy[edge[0]] = maxsize
+        m_copy[edge[0][1], edge[0][0]] = maxsize
+        m_copy[edge[0][0], edge[0][0]] = maxsize
+
+    min_cost = m_copy.min()
+    min_index = np.where(m_copy == min_cost)
+    min_edge = ((min_index[0][0], min_index[1][0]), min_cost)
+
+    neighbours1 = tu.get_neighbour_nodes(tour, min_index[0][0])
+    neighbours2 = tu.get_neighbour_nodes(tour, min_index[1][0])
+    # print(neighbours1)
+    # print(neighbours2)
+    # print(neighbours1[0][0], neighbours2[0][0])
+    # print(neighbours1[1][1], neighbours2[1][1])
+
+    cost1 = m[neighbours1[0][0], neighbours2[0][0]]
+    cost2 = m[neighbours1[1][1], neighbours2[1][1]]
+
+    min_edge2 = None
+    if cost1 < cost2:
+        min_edge2 = ((neighbours1[0][0], neighbours2[0][0]), cost1)
+    else:
+        min_edge2 = ((neighbours1[1][1], neighbours2[1][1]), cost2)
+
+    # print(min_edge)
+    # print(min_edge2)
+
+    tour.append(min_edge)
+    tour.append(min_edge2)
+
+    to_be_removed = ((min_edge[0][0], min_edge2[0][0]), m[(min_edge[0][0], min_edge2[0][0])])
+    if to_be_removed in tour:
+        tour.pop(tour.index(to_be_removed))
+    else:
+        to_be_removed = ((min_edge2[0][0], min_edge[0][0]), m[(min_edge[0][0], min_edge2[0][0])])
         if to_be_removed in tour:
             tour.pop(tour.index(to_be_removed))
         else:
-            to_be_removed = ((min_edge2[0][0], min_edge[0][0]), m[(min_edge[0][0], min_edge2[0][0])])
+            to_be_removed = ((min_edge[0][0], min_edge2[0][1]), m[(min_edge[0][0], min_edge2[0][1])])
             if to_be_removed in tour:
                 tour.pop(tour.index(to_be_removed))
             else:
-                to_be_removed = ((min_edge[0][0], min_edge2[0][1]), m[(min_edge[0][0], min_edge2[0][1])])
+                to_be_removed = ((min_edge2[0][1], min_edge[0][0]), m[(min_edge[0][0], min_edge2[0][1])])
                 if to_be_removed in tour:
                     tour.pop(tour.index(to_be_removed))
-                else:
-                    to_be_removed = ((min_edge2[0][1], min_edge[0][0]), m[(min_edge[0][0], min_edge2[0][1])])
-                    if to_be_removed in tour:
-                        tour.pop(tour.index(to_be_removed))
 
-        # print(to_be_removed)
-        to_be_removed = ((min_edge[0][1], min_edge2[0][1]), m[(min_edge[0][1], min_edge2[0][1])])
+    # print(to_be_removed)
+    to_be_removed = ((min_edge[0][1], min_edge2[0][1]), m[(min_edge[0][1], min_edge2[0][1])])
+    if to_be_removed in tour:
+        tour.pop(tour.index(to_be_removed))
+    else:
+        to_be_removed = ((min_edge2[0][1], min_edge[0][1]), m[(min_edge[0][1], min_edge2[0][1])])
         if to_be_removed in tour:
             tour.pop(tour.index(to_be_removed))
         else:
-            to_be_removed = ((min_edge2[0][1], min_edge[0][1]), m[(min_edge[0][1], min_edge2[0][1])])
+            to_be_removed = ((min_edge[0][1], min_edge2[0][0]), m[(min_edge[0][1], min_edge2[0][0])])
             if to_be_removed in tour:
                 tour.pop(tour.index(to_be_removed))
             else:
-                to_be_removed = ((min_edge[0][1], min_edge2[0][0]), m[(min_edge[0][1], min_edge2[0][0])])
+                to_be_removed = ((min_edge2[0][0], min_edge[0][1]), m[(min_edge[0][1], min_edge2[0][0])])
                 if to_be_removed in tour:
                     tour.pop(tour.index(to_be_removed))
-                else:
-                    to_be_removed = ((min_edge2[0][0], min_edge[0][1]), m[(min_edge[0][1], min_edge2[0][0])])
-                    if to_be_removed in tour:
-                        tour.pop(tour.index(to_be_removed))
-        # print(to_be_removed)
+    # print(to_be_removed)
 
-        tour = tu.sort_tour_list(m, tour)
+    tour = tu.sort_tour_list(m, tour)
 
-        new_cost = tu.tour_cost_sorted(tour)
-        # tu.print_tour_sorted(tour)
+    new_cost = tu.tour_cost_sorted(tour)
+    # tu.print_tour_sorted(tour)
 
-        print('Total Cost: ', new_cost)
+    print('Total Cost: ', new_cost)
 
-        if new_cost < best_cost:
-            best_cost = new_cost
-            best_tour = deepcopy(tour)
-        else:
-            print('Best Cost Reached:', best_cost)
-            break
+    if new_cost < best_cost:
+        best_cost = new_cost
+    #     best_tour = deepcopy(tour)
+    # else:
+    #     print('Best Cost Reached:', best_cost)
+    #     break
 
-    return best_tour, best_cost
+    return tour, best_cost
 
 
-def ts_1010(m, tour, best):
-    best_cost = best
-    best_tour = deepcopy(tour)
+def ts_1010(m, tour, best_cost):
+    # best_cost = best
+    # best_tour = deepcopy(tour)
 
-    while True:
-        m_copy = deepcopy(m)
+    # while True:
+    m_copy = deepcopy(m)
 
-        for edge in tour:
-            m_copy[edge[0]] = maxsize
-            m_copy[edge[0][1], edge[0][0]] = maxsize
-            m_copy[edge[0][0], edge[0][0]] = maxsize
+    for edge in tour:
+        m_copy[edge[0]] = maxsize
+        m_copy[edge[0][1], edge[0][0]] = maxsize
+        m_copy[edge[0][0], edge[0][0]] = maxsize
 
-        min_cost = m_copy.min()
-        min_index = np.where(m_copy == min_cost)[0]
-        min_edge1 = ((min_index[0], min_index[1]), min_cost)
+    min_cost = m_copy.min()
+    min_index = np.where(m_copy == min_cost)
+    min_edge1 = ((min_index[0][0], min_index[1][0]), min_cost)
 
-        neighbours = tu.get_neighbour_nodes(tour, min_index[0])
-        neighbours += tu.get_neighbour_nodes(tour, min_index[1])
+    neighbours = tu.get_neighbour_nodes(tour, min_index[0][0])
+    neighbours += tu.get_neighbour_nodes(tour, min_index[1][0])
 
-        # print(min_edge1)
-        # print(neighbours)
+    # print(min_edge1)
+    # print(neighbours)
 
-        max_edge1 = ((-1, -1), -1)
-        for edge in neighbours:
-            if m[edge] > max_edge1[1] and not tu.are_neighbours(edge, max_edge1[0]):
-                max_edge1 = (edge, m[edge])
+    max_edge1 = ((-1, -1), -1)
+    for edge in neighbours:
+        if m[edge] > max_edge1[1] and not tu.are_neighbours(edge, max_edge1[0]):
+            max_edge1 = (edge, m[edge])
 
-        max_edge2 = neighbours[(neighbours.index(max_edge1[0]) + 2) % 4]
-        if neighbours.index(max_edge1[0]) != 3:
-            min_edge2 = max_edge1[0][0], max_edge2[0]
-        else:
-            min_edge2 = max_edge1[0][1], max_edge2[1]
+    max_edge2 = neighbours[(neighbours.index(max_edge1[0]) + 2) % 4]
+    if neighbours.index(max_edge1[0]) != 3:
+        min_edge2 = max_edge1[0][0], max_edge2[0]
+    else:
+        min_edge2 = max_edge1[0][1], max_edge2[1]
 
-        max_edge2 = (max_edge2, m[max_edge2])
-        min_edge2 = (min_edge2, m[min_edge2])
+    max_edge2 = (max_edge2, m[max_edge2])
+    min_edge2 = (min_edge2, m[min_edge2])
 
-        # print()
-        # print('add', min_edge1)
-        # print('remove', max_edge1)
-        # print('add', min_edge2)
-        # print('remove', max_edge2)
+    # print()
+    # print('add', min_edge1)
+    # print('remove', max_edge1)
+    # print('add', min_edge2)
+    # print('remove', max_edge2)
 
-        tour.append(min_edge1)
-        tour.pop(tour.index(max_edge1))
-        tour.append(min_edge2)
-        tour.pop(tour.index(max_edge2))
+    tour.append(min_edge1)
+    tour.pop(tour.index(max_edge1))
+    tour.append(min_edge2)
+    tour.pop(tour.index(max_edge2))
 
-        tour = tu.sort_tour_list(m, tour)
+    tour = tu.sort_tour_list(m, tour)
 
-        new_cost = tu.tour_cost_sorted(tour)
-        # tu.print_tour_sorted(tour)
+    new_cost = tu.tour_cost_sorted(tour)
+    # tu.print_tour_sorted(tour)
 
-        print('Total Cost: ', new_cost)
+    print('Total Cost: ', new_cost)
 
-        if new_cost < best_cost:
-            best_cost = new_cost
-            best_tour = deepcopy(tour)
-        else:
-            print('Best Cost Reached:', best_cost)
-            break
+    if new_cost < best_cost:
+        best_cost = new_cost
+    #     best_tour = deepcopy(tour)
+    # else:
+    #     print('Best Cost Reached:', best_cost)
+    #     break
 
-    return best_tour, best_cost
+    return tour, best_cost
